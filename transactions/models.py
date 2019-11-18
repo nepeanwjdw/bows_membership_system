9050c605536d45553c3244372f8833c53c8fe1ba
Will Wayman <will.jdw@gmail.com>
2019-11-18 17:16:15 +0000

    added models

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TOP_UP = 'TU'
+    PURCHASE = 'PU'
+    TYPE_CHOICES = [
+        (TOP_UP, 'Top up'),
+        (PURCHASE, 'Purchase food or drink'),
+    ]
+
+    card_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns the transaction amount"""
+        return self.amount
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.IntegerField(primary_key=True)
+    card_id = models.CharField(max_length=16, unique=True)
+    balance = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    mobile_number = models.CharField(max_length=30)
+
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['card_id']
+
+    def __str__(self):
+        """Returns the user's full name."""
+        return '%s %s' % (self.first_name, self.last_name)
+
+    def balance(self):
+        """Returns the user's balance"""
+        return self.balance
