c3e2eb29e958c7ff165f859c2c20f304b6b5f93a
Will Wayman <will.jdw@gmail.com>
2019-11-19 16:19:30 +0000

    added regex to validate card id in request url

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('users/<card_id>/', views.RetrieveEmployee.as_view()),
+    re_path(r'^users/(?P<card_id>[a-zA-Z0-9]{16})/$', views.RetrieveEmployee.as_view()),
 ]
