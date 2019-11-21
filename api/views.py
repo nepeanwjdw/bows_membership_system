abeb61dece6e2a711e65d848d12afca024f9d86e
Will Wayman <will.jdw@gmail.com>
2019-11-21 14:57:51 +0000

    retrieve employee view doesn't require authentication

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from users.models import Employee
 from . import serializers
 
@@ -7,6 +8,7 @@ class RetrieveEmployee(generics.RetrieveAPIView):
     queryset = Employee.objects.all()
     serializer_class = serializers.EmployeeSerializer
     lookup_field = 'card_id'
+    permission_classes = [AllowAny]
 
 
 class TopUpBalance(generics.UpdateAPIView):
